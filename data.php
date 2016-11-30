ab4db49bc659463ed00484c39a98f010b49eeba7
“Marliis <marliisodamus@users.noreply.github.com>
2016-11-30 09:22:26 +0200

    lisasin

---
+++ data.php
@@ -0,0 +1,22 @@
+<?php 
+	require("functions.php");
+	
+	// RÜHMATÖÖ
+	// kas on sisseloginud, kui ei ole siis
+	// suunata login lehele
+	if (!isset ($_SESSION["userId"])) {
+		
+		header("Location: login.php");
+		
+	}
+	
+	//kas ?logout on aadressireal
+	if (isset($_GET["logout"])) {
+		
+		session_destroy();
+		
+		header("Location: login.php");
+		
+	}
+	
+?>
--- functions.php
+++ functions.php
@@ -1,1 +1,86 @@
-..
+<?php
+	
+	// RÜHMATÖÖ
+	//functions.php
+	require("../../config.php");
+	
+	session_start();
+	$database = "if16_health_diary";
+
+
+function signup ($name, $gender, $age, $email, $password) {
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"],$GLOBALS["serverUsername"],$GLOBALS["serverPassword"],$GLOBALS["database"]);
+		$stmt = $mysqli->prepare("INSERT INTO user_sample (name, gender, age, email, password) VALUES (?, ?, ?, ?, ?)");
+		echo $mysqli->error;
+		//asendan küsimärgi väärtustega
+		//iga muutuja kohta tuleb kirjutada üks täht, mis tüüpi muutuja on
+		//s-stringi
+		//i-integer
+		//d-double/float
+		$stmt->bind_param("sisss", $name, $age, $email, $password, $gender);
+		
+		if ($stmt->execute()) {
+			echo "salvestamine õnnestus";
+		} else {
+			echo "ERROR ".$stmt->error;
+		}
+		
+	}
+	
+	
+	function login($email, $password) {
+		
+		$error = "";
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"],$GLOBALS["serverUsername"],$GLOBALS["serverPassword"],$GLOBALS["database"]);
+		$stmt = $mysqli->prepare("
+			SELECT id, email, password, created 
+			FROM user_sample
+			WHERE email = ?
+		");
+		echo $mysqli->error;
+		
+		//asendan küsimärgi
+		$stmt->bind_param("s", $email);
+		
+		//määran tupladele muutujad
+		$stmt->bind_result($id, $emailFromDb, $passwordFromDb, $created);
+		$stmt->execute();
+		
+		//küsin rea andmeid
+		if($stmt->fetch()) {
+			//oli rida
+		
+			// võrdlen paroole
+			$hash = hash("sha512", $password);
+			if($hash == $passwordFromDb) {
+				
+				echo "kasutaja ".$id." logis sisse";
+				
+				
+				$_SESSION["userId"] = $id;
+				$_SESSION["email"] = $emailFromDb;
+				
+				//suunaks uuele lehele
+				header("Location: data.php");
+				exit();
+				
+			} else {
+				$error = "parool vale";
+			}
+			
+		
+		} else {
+			//ei olnud 
+			
+			$error = "sellise emailiga ".$email." kasutajat ei olnud";
+		}
+		
+		
+		return $error;
+		
+		
+	}
+
+?>
