1ef4741f9f96e8c8654573cbeb47a48ea0625a72
Regiina Krivulina <regiinakrivulina@users.noreply.github.com>
2016-12-07 08:39:49 +0200

    Lisatud väljad

--- page/data.php
+++ page/data.php
@@ -22,13 +22,28 @@
 		exit();
 	}
 	
+	if( isset($_POST["date"]) &&
+		isset($_POST["weight"]) &&
+		isset($_POST["height"]) &&
+		isset($_POST["training"]) &&
+		isset($_POST["food"]) &&
+		!empty($_POST["dateError"]) &&
+		!empty($_POST["weightError"]) &&
+		!empty($_POST["heightError"]) &&
+		!empty($_POST["trainingError"]) &&
+		!empty($_POST["foodError"])
+	){
+			
+	$date = "";
+	$dateError = "";
+	
 			$date =  new DateTime($_POST['date']);
 			$date =  $date->format('Y-m-d');
 	
 ?>
 <h1>Kasutaja andmed</h1>
 <p>
-	Tere tulemast <a href="user.php"><?=$_SESSION["userEmail"];?>!</a>
+	Tere tulemast <a href="user.php">!</a>
 	<a href="?logout=1">Logi välja</a>
 </p>
 
@@ -38,6 +53,23 @@
 		<input name="date" type="date" value="<?=$date;?>"><?php echo $dateError;?>
 		<br><br>
 		
+		<label>Sisesta kaal</label><br>
+		<input name="weight" type="weight" value="<?=$weight;?>"><?php echo $weightError;?>
+		<br><br>
+		
+		<label>Sisesta pikkus</label><br>
+		<input name="height" type="height" value="<?=$height;?>"><?php echo $heightError;?>
+		<br><br>
+		
+		<label>Lisa treening</label><br>
+		<input name="training" type="training" value="<?=$training;?>"><?php echo $trainingError;?>
+		<br><br>
+		
+		<label>Lisa söögikord</label><br>
+		<input name="food" type="food" value="<?=$food;?>"><?php echo $foodError;?>
+		<br><br>
+		
+		<input type="submit" value="Salvesta">
 </form>
 
 
@@ -46,4 +78,4 @@
 <br>
 <br>
 <br>
-<?php require("../footer.php"); ?>
+<?php require("../footer.php");?>
