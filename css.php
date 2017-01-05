d1183466e5e4ea17af5ac7f25a238466a9604817
“Marliis <marliisodamus@users.noreply.github.com>
2017-01-05 23:26:14 +0200

    added css.php file

---
+++ css.php
@@ -0,0 +1,45 @@
+<style>
+	.background {
+		background-repeat: repeat;
+		background-size: contain; 
+		background-size: 100%;
+		bottom: 0;
+		right: 0;
+		background-position: right 100%;
+		padding: 0 1em;
+		position: relative;
+	}
+	
+	.data_background {
+		background-color: rgb(255, 255, 255);
+		background-size: 50%;
+		background-repeat: no-repeat;
+		background-position: right 95%;
+		padding: 0 1em;
+		position: relative;
+
+	
+	}
+	
+	.addTraining_background {
+		background-color: rgb(251, 252, 254);
+		background-size: 45%;
+		background-repeat: no-repeat;
+		background-position: 650px 250px;
+		background-position: right 95%;
+		padding: 0 1em;
+		position: relative;
+	}
+	
+	.addFood_background {
+		background-color: rgb(255, 255, 255);
+		background-size: 50%;
+		background-repeat: no-repeat;
+		background-position: 600px 350px;
+		background-position: right 95%;
+		padding: 0 1em;
+		position: relative;
+
+	}
+	
+</style>
--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Logi sisse või loo kasutaja</title>
+	<title>Minu tervisepäevik</title>
 	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	
