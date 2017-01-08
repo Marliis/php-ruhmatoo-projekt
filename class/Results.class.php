e006e6c243fe9f57a8a35f34579015b77fc67eaf
geithy <geithy@users.noreply.github.com>
2017-01-08 21:04:56 +0200

    tulemuste leht ja treeningute andmebaasi salvestamine

--- class/Results.class.php
+++ class/Results.class.php
@@ -10,24 +10,121 @@ class Food {
 		
 	}
 
-	function displayFood() {
-		// Check connection
+	// funktsioon kuupäevade järjestamiseks arrays usortiga
+	function orderByDate($array){
+
+		function date_compare($a, $b){
+			$t1 = strtotime($a['details']['created']);
+			$t2 = strtotime($b['details']['created']);
+			return $t2 - $t1;
+		}
+		usort($array, 'date_compare');
+
+		return $array;
+
+	}
+
+	// kuupäevad valides saadame uuele aadressile
+	function search($startDate, $endDate){
+
+		header("Location: checkTraining.php?startDate=" . $startDate . "&endDate=" . $endDate);
+
+	}
+
+	function displayResults($startDate, $endDate){
+
+		// kontrollime ühendust
 		if ($this->connection->error) {
 		    die("Connection failed");
-		} 
-
-		$sql = "SELECT food, content, drinks, amount, user_id FROM FoodData";
-
-		$result = $this->connection->query($sql);
-
-		if ($result->num_rows > 0) {
-		    // output data of each row
-		    while($row = $result->fetch_assoc()) {
-				echo "TOIT:" . $row["food"]. ", SISU: " . $row["content"]. ", JOOGID: " . $row["drinks"]. ", KOGUS: " . $row["amount"]. "<br>";
-		    }
-		} else {
-		    echo "Tulemused puuduvad";
 		}
+
+		// kui otsime kuupäevadega
+		if (!empty($startDate) && !empty($endDate)){
+
+			$sql_extra = " AND created BETWEEN '" . $startDate . "' AND '" . $endDate . "'";
+
+		}
+
+		$array = "";
+
+		// toit arraysse
+		$sql = "SELECT user_id, food, content, drinks, amount, created FROM FoodData WHERE user_id = " . $_SESSION["userId"] . $sql_extra;
+		$result = $this->connection->query($sql);
+		if ($result->num_rows > 0) {
+		    // iga toit arraysse
+		    while($row = $result->fetch_assoc()) {
+
+				$array[$row["created"]] = array("type" => "food",
+
+							"details" =>
+
+							array(	"food" => $row["food"],
+									"content" => $row["content"],
+									"drinks" => $row["drinks"],
+									"amount" => $row["amount"],
+									"created" => $row["created"]
+							)
+				);
+
+		    }
+		}
+
+		// persondata arraysse
+		$sql = "SELECT user_id, weight, height, created FROM PersonData WHERE user_id = " . $_SESSION["userId"] . $sql_extra;
+		$result = $this->connection->query($sql);
+		if ($result->num_rows > 0) {
+			// iga persondata arraysse
+			while($row = $result->fetch_assoc()) {
+
+				$array[$row["created"]] = array("type" => "person",
+
+					"details" =>
+
+						array(	"weight" => $row["weight"],
+								"height" => $row["height"],
+								"created" => $row["created"]
+						)
+				);
+
+			}
+		}
+
+		// treenngud arraysse
+		$sql = "SELECT user_id, TypeOfTraining, WorkoutHours, Kilometers, feeling, comment, created FROM AthleteData_2 WHERE user_id = " . $_SESSION["userId"] . $sql_extra;
+		$result = $this->connection->query($sql);
+		if ($result->num_rows > 0) {
+			// iga treening arraysse
+			while($row = $result->fetch_assoc()) {
+
+				$array[$row["created"]] = array("type" => "training",
+
+					"details" =>
+
+						array(	"TypeOfTraining" => $row["TypeOfTraining"],
+								"WorkoutHours" => $row["WorkoutHours"],
+								"Kilometers" => $row["Kilometers"],
+								"feeling" => $row["feeling"],
+								"comment" => $row["comment"],
+								"created" => $row["created"]
+						)
+				);
+
+			}
+		}
+
+		// kui array pole tühi paneme kuupäevade järgi õigesse järjekorda
+		if(!empty($array)){
+			$array = self::orderByDate($array);
+		} else {
+			// kui ei leidnud tabelist tulemusi saadame info selle kohta
+			$array = array("type" => "not_found", "detail" => array("error" => "empty"));
+
+		}
+
+		// saadame info, et tabel joonistada
+		return $array;
+
+
 	}
 }
 ?>
--- page/checkTraining.php
+++ page/checkTraining.php
@@ -7,7 +7,8 @@
 	
 	require("../class/Helper.class.php");
 	$Helper = new Helper($mysqli);
-	
+
+	require("../css.php");
 	
 	//kui ei ole kasutaja id'd
 	if (!isset($_SESSION["userId"])){
@@ -26,20 +27,127 @@
 		exit();
 
 	}
+
+	//var_dump($_POST);
+	if( !empty($_POST["startDate"]) &&
+		!empty($_POST["endDate"])
+	) {
+		$Results->search($Helper->cleanInput($_POST["startDate"]), $Helper->cleanInput($_POST["endDate"]));
+	}
+
 ?>
 
 <?php require("../header.php"); ?>
 
+	<body class="addTraining_background" background="../addTraining_page.jpeg">
+
 	<div class="container">
-	
+
 		<div class="row">
+
 			<div class="col-sm-9">
-				<?php
-					$Results->displayFood();
-				?>
+				<br>
+				<?=$msg;?>
+				<p>
+					<a href="addTraining.php"><?=$_SESSION["userEmail"];?></a>
+				</p>
+				<h3><a href="data.php"> < Mine tagasi</a></h3>
+				<h1>Tulemused</h1>
+	
+				<div class="row">
+					<div class="col-sm-9">
+
+						<form method="POST">
+
+							<div class="row">
+								<div class="col-md-4">
+									<label>Alguskuupäev</label><br>
+									<input name="startDate" type="date" value="<?php if(isset($_GET["startDate"])){ echo $_GET["startDate"]; } ?>">
+								</div>
+								<div class="col-md-4">
+									<label>Lõpukuupäev</label><br>
+									<input name="endDate" type="date" value="<?php if(isset($_GET["endDate"])){ echo $_GET["endDate"]; } ?>">
+								</div>
+								<div class="col-md-4">
+									<br />
+									<input class="btn btn-success btn-sm hidden-xs" type="submit" value="Otsi">
+									<?php if(isset($_GET["startDate"]) or isset($_GET["endDate"])){ ?>
+										<a class="btn btn-danger btn-sm hidden-xs" href="checkTraining.php">Taasta</a>
+									<?php } ?>
+								</div>
+							</div>
+
+
+						</form>
+
+						<table class="table table-bordered">
+							<tbody>
+							<?php
+
+							$startDate = $Helper->cleanInput($_GET["startDate"]);
+							$endDate = $Helper->cleanInput($_GET["endDate"]);
+
+								// võtame tulemused
+								$array = $Results->displayResults($startDate, $endDate);
+
+								// kui pole tulemusi
+								if($array["type"] == "not_found"){
+
+									?>
+									<tr class="error">
+										<td><small>Tulemusi ei leitud!</small></td>
+									</tr>
+									<?php
+									// kui leidis tulemusi
+								} else {
+
+									foreach($array as $result){
+
+											$type = $result["type"];
+											if($type == "food"){ $typeText = "Söögikord"; }
+											if($type == "person"){ $typeText = "Andmed"; }
+											if($type == "training"){ $typeText = "Treening"; }
+
+											$details = $result["details"];
+
+											$phpdate = strtotime( $details["created"] );
+											$date = date( 'd.m.Y', $phpdate );
+
+											?>
+											<tr class="<?php echo $type; ?>">
+												<td class="text-center"><img src="../<?php echo $type; ?>.png"/></td>
+												<td><small><?php echo $date; ?></small><br /><?php echo $typeText; ?></td>
+												<?php if($type == "food"){ ?>
+												<td><small><?php echo $details["food"]; ?></small><br /><?php echo $details["content"]; ?></td>
+												<td><small><?php echo $details["drinks"]; ?></small><br /><?php echo $details["amount"]; ?>l</td>
+												<?php } ?>
+												<?php if($type == "person"){ ?>
+												<td><small>Kaal</small><br /><?php echo $details["weight"]; ?>kg</td>
+												<td><small>Pikkus</small><br /><?php echo $details["height"]; ?>cm</td>
+												<?php } ?>
+												<?php if($type == "training"){ ?>
+													<td><small><?php echo $details["TypeOfTraining"]; ?></small><br /><?php echo $details["WorkoutHours"]; ?>h - <?php echo $details["Kilometers"]; ?>km</td>
+													<td><small><?php echo $details["feeling"]; ?></small> - <small><?php echo $details["comment"]; ?></small></td>
+												<?php } ?>
+											</tr>
+											<?php
+
+										}
+
+									}
+							?>
+							</tbody>
+						</table>
+
+					</div>
+				</div>
+
 			</div>
+
 		</div>
-<br>
+
+	</div>
+		<br>
 <br>
 <br>
 <br>
